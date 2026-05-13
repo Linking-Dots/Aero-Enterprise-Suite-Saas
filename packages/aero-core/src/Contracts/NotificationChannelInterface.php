54950df306b1a413612a80be71d583e9d70e5a88
Emam Hosen <emamhsajeeb@gmail.com>
2026-05-13 20:53:22 +0600

    feat(aero-core): add aero/contracts ^1.0 dep + version 1.0.0; delete 18 moved contract files -- clean cut, no aliases

--- packages/aero-core/src/Contracts/NotificationChannelInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Aero\Core\Contracts;
-
-interface NotificationChannelInterface
-{
-    public function send(object $notifiable, object $notification): void;
-    public function channelName(): string;
-}
